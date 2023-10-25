2d728c685a1f9cfaaa003cd7fbd4fd11060fc199
vinicius-novo <viniciusnovo.enge@gmail.com>
2023-10-24 22:57:26 -0300

    quarto commit

--- desafio01.js
+++ desafio01.js
@@ -13,3 +13,27 @@
 
 // Ao final deve se exibir uma mensagem:
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+let nome = "Vinicius"
+let xp = 2500
+let nivel = ""
+
+if (xp <= 1000) {
+  nivel = "Ferro"
+} else if (xp <= 2000) {
+  nivel = "Bronze"
+} else if (xp <= 5000) {
+  nivel = "Prata"
+} else if (xp <= 7000) {
+  nivel = "Ouro"
+} else if (xp <= 8000) {
+  nivel = "Platina"
+} else if (xp <= 9000) {
+  nivel = "Ascendente"
+} else if (xp <= 10000) {
+  nivel = "Imortal"
+} else if (xp >= 10001) {
+  nivel = "Radiante"
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
